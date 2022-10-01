713ea6fb8cd3b9eb50c413583f84a7384c8d01f6
Joshua <73680704+UrFingPoor@users.noreply.github.com>
2022-09-30 21:35:17 -0400

    Update Config.cs

--- API/Config.cs
+++ API/Config.cs
@@ -2,7 +2,7 @@ namespace SpacewareRBX
 {
     public static class Config
     {
-        public static string Host = "https://cdn.wearedevs.net/";
+        public static readonly string Host = "https://cdn.wearedevs.net/";
         public static string Patched { get; set; }
         public static string Version { get; set; }
         public static string Injector { get; set; }
